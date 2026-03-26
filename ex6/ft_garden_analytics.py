3d1a8141b963425184c8c0c031e5a1c30bb2b995
jennyx21 <jtruckse@student.42heilbronn.de>
2026-03-26 18:41:59 +0100

    test eval

--- ex6/ft_garden_analytics.py
+++ ex6/ft_garden_analytics.py
@@ -1,32 +1,32 @@
 
-class Plant:
-    def __init__(self, name: str, height: int, days: int):
-        self.name = name
-        self.height = height
-        self.days = days
+# class Plant:
+#     def __init__(self, name: str, height: int, days: int):
+#         self.name = name
+#         self.height = height
+#         self.days = days
 
-    def grow(self, days: int):
-        self.height += days
-        print(f"{self.name} grew {days}cm")
+#     def grow(self, days: int):
+#         self.height += days
+#         print(f"{self.name} grew {days}cm")
 
-    def get_info(self):
-        return f"{self.name}: {self.height}cm, {self.days} days old"
+#     def get_info(self):
+#         return f"{self.name}: {self.height}cm, {self.days} days old"
 
+# class FloweringPlant(Plant):
+#     def __init__(self, name: str, height: int, days: int, color: str):
+#         super().__init__(name, height, days)
+#         self.color = color
 
-class FloweringPlant(Plant):
-    def __init__(self, name: str, height: int, days: int, color: str):
-        super().__init__(name, height, days)
-        self.color = color
+#     def display_info(self):
+#         print(f"{self.name}: {self.height}cm, {self.color} flower
+# (blooming)")
 
-    def display_info(self):
-        print(f"{self.name}: {self.height}cm, {self.color} flower (blooming)")
+# class PrizeFlower(FloweringPlant):
+#     def __init__(self, name: str, height: int,
+#                  days: int, color: str, points: int):
+#         super().__init__(name, height, days, color)
+#         self.points = points
 
-
-class PrizeFlower(FloweringPlant):
-    def __init__(self, name: str, height: int,
-                 days: int, color: str, points: int):
-        super().__init__(name, height, days, color)
-        self.points = points
-
-    def diplay_info(self):
-        print(f"{self.name}: {self.height}cm, {self.color} flower (blooming)")
+#     def diplay_info(self):
+#         print(f"{self.name}: {self.height}cm, {self.color} flower
+# (blooming)")
